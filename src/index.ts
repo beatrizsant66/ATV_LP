299efba724275dc14f1c85990f1055f26a822066
Beatriz Santos <beatriz.etec2019@gmail.com>
2026-05-01 15:17:50 -0300

    implementa cadastro e remoção de livros

--- src/index.ts
+++ src/index.ts
@@ -46,3 +46,31 @@ function exibirBiblioteca(): void{
 }
 
 exibirBiblioteca();
+
+function adicionarLivro(titulo: string, autor: string, ano: number, totalPaginas: number): void{
+    titulos.push(titulo);
+    autores.push(autor);
+    anos.push(ano);
+    paginas.push(totalPaginas);
+    lido.push(false);
+    avaliacoes.push(0);
+}
+
+function deletarLivro(indice: number): void{
+    if(indice >= 0 && indice <= titulos.length){
+        titulos.splice(indice, 1);
+        autores.splice(indice, 1);
+        anos.splice(indice, 1);
+        paginas.splice(indice, 1);
+        lido.splice(indice, 1);
+        avaliacoes.splice(indice, 1);
+    }
+}
+
+adicionarLivro("Hamlet","William Shakespeare",1623,192);
+adicionarLivro("A Revolução dos Bichos","George Orwell",1945,112);
+
+deletarLivro(4);
+
+exibirBiblioteca();
+
